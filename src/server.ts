64457a828b4012d4e283a28fef8a27031ddca167
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-19 06:51:23 +0200

    Server + DB setup

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import app from "./app";
+import { connectDB } from "./config/db.config";
+
+const PORT = process.env.PORT || 4000;
+
+async function startServer() {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+startServer();
